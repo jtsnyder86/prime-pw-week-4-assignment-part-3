4bc8143e11be65ddf8596035b21761e96f3d7621
Josh Snyder <joshua.t.snyder@gmail.com>
2022-07-03 16:55:02 -0500

    made some more changes but couldn't get some figured out

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -9,3 +9,28 @@ function addItem (item) {
     return true
 }
 addItem('Bignet');
+addItem('Cookie Dough');
+addItem('Gas station sushi');
+addItem('Milk');
+addItem('Healthy stuff');
+console.log(`Basket is ${basket}`);
+console.log('Adding bingets', addItem('bingets'));
+console.log('Adding cookie dough', addItem('cookie dough'));
+console.log('Adding gas station sushi', addItem('gas station sushi'));
+console.log('Adding milk', addItem('milk'));
+console.log('Adding healthy stuff', addItem('Healthy stuff'));
+
+function listItems() {
+    for (let item of basket) {
+        console.log(item);
+    }
+}
+
+listItems();
+
+function empty() {
+    basket = [];
+    //basket.pop(item)
+}
+
+console.log(`Basket is now, ${basket}`);
